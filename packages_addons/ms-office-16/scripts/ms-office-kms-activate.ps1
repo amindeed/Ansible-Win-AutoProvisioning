51a9e3b8daca05c3380fe55c1f32e039405f3a84
Amine AL KADERI <6241545+amindeed@users.noreply.github.com>
2023-09-26 10:20:00 +0100

    [2023-09-26 10:20:52] Auto-commit

---
+++ packages_addons/ms-office-16/scripts/ms-office-kms-activate.ps1
@@ -0,0 +1,55 @@
+# On artifacts server (e.g. generic Artifactory repository), this file should be placed
+# under the same directory as 'Microsoft Office 2016 x86' installer bundle file;
+# (`ms-office-16/scripts/ms-office-kms-activate.ps1`)
+# kms.server.local
+
+$msOfficeInstallPath = "C:\Program Files (x86)\Microsoft Office\Office16"
+$kmsserver = $args[0]
+
+try {
+    # Check if Office is already activated
+    $checkOfficeActivation = 'C:\Windows\SysWOW64\Cscript.exe "$msOfficeInstallPath\ospp.vbs" /dstatus'
+    $checkOutput = Invoke-Expression -Command $checkOfficeActivation
+    $lines = $checkOutput -split [Environment]::NewLine
+    $licenseStatusLine = $lines | Where-Object { $_ -match "^LICENSE STATUS:" }
+    $kmsOverrideLine = $lines | Where-Object { $_ -match "^\s*KMS machine registry override defined:" }
+
+    if (($licenseStatusLine -match "---LICENSED---") -and ($kmsOverrideLine -match $kmsserver)) {
+        Write-Host "OK_NO_CHANGE"
+    } else {
+
+        $DelKMSHost = (Start-Process -FilePath "C:\Windows\SysWOW64\Cscript.exe" -ArgumentList "'$msOfficeInstallPath\ospp.vbs' /remhst" -Wait -PassThru).ExitCode
+        #if ($DelKMSHost -ne 0) { 
+        #    Write-Host "[KO_FAILURE] Failed to remove old KMS Server settings."
+        #    exit 0
+        #}
+
+        $SetKMSHost = (Start-Process -FilePath "C:\Windows\SysWOW64\Cscript.exe" -ArgumentList "`"$msOfficeInstallPath\ospp.vbs`" /sethst:$kmsserver" -Wait -PassThru).ExitCode
+        if ($SetKMSHost -ne 0) { 
+            Write-Host "[KO_FAILURE] Failed to set KMS Server."
+            exit 0
+        }
+
+        $ActivateMSOffice = (Start-Process -FilePath "C:\Windows\SysWOW64\Cscript.exe" -ArgumentList "`"$msOfficeInstallPath\ospp.vbs`" /act" -Wait -PassThru).ExitCode
+        if ($ActivateMSOffice -ne 0) { 
+            Write-Host "[KO_FAILURE] Failed to activate Office 2016."
+            exit 0
+        }
+
+        $checkOutput = Invoke-Expression -Command $checkOfficeActivation
+        $lines = $checkOutput -split [Environment]::NewLine
+        $licenseStatusLine = $lines | Where-Object { $_ -match "^LICENSE STATUS:" }
+        $kmsOverrideLine = $lines | Where-Object { $_ -match "^\s*KMS machine registry override defined:" }
+
+        if (($licenseStatusLine -match "---LICENSED---") -and ($kmsOverrideLine -match $kmsserver)) {
+            Write-Host "OK_CHANGE"
+        } else {
+            Write-Host "[KO_FAILURE] MS Office not activated: `"$licenseStatusLine`""
+        }
+    }
+} catch {
+    $errorMessage = $_.Exception.Message
+    Write-Host "Unexpected exception/error: $errorMessage"
+    exit 1
+}
+
--- tools/check_software_installed.ps1
+++ tools/check_software_installed.ps1
@@ -1,11 +1,18 @@
-$appName = "Microsoft Office Standard 2016"
+# Run this script on a Windows machine having the software in question already installed,
+# in order to detremine its name, as this should be provided in the playbook file, 
+# as a value of 'setup.installations.software_components[0].software_fullname' for instance,
+# to better process the setup.
+
+$appName = "Software Name" # e.g.: $appName = "Microsoft Office Standard 2016"
 
 $wmiQuery = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "*$appName*" }
 $regPath1 = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
 $regPath2 = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+$regPath3 = "HKLM:\SOFTWARE"
 $regCheck1 = Get-ChildItem -Path $regPath1 | Where-Object { $_.GetValue("DisplayName") -like "*$appName*" }
 $regCheck2 = Get-ChildItem -Path $regPath2 | Where-Object { $_.GetValue("DisplayName") -like "*$appName*" }
-if ($wmiQuery -or $regCheck1 -or $regCheck2) { $status = "Installed" } else { $status = "Not Installed" }
+$regCheck3 = Get-ChildItem -Path $regPath3 | Where-Object { $_.GetValue("DisplayName") -like "*$appName*" }
+if ($wmiQuery -or $regCheck1 -or $regCheck2 -or $regCheck3) { $status = "Installed" } else { $status = "Not Installed" }
 
 Write-Host "`n`n$appName is $status`n`n"
 
